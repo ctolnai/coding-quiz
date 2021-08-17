4341a6fda98da7125e71b952cceee4ad80fd9bac
Chris Tolnai <christolnai@gmail.com>
2021-08-16 17:52:19 -0700

    got buttons to be hidden when page loads, then shows up after hitting Start

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,25 +8,30 @@ var answer3 = document.querySelector("#answer3");
 var correctwrong = document.querySelector("#correctwrong");
 var timer = document.querySelector("#timer");
 var container = document.querySelector(".container");
+var display = document.querySelector(".answer")
+
 
 document.getElementById("title").textContent = "Coding Quiz Challenge";
 document.getElementById("question").textContent = "Try to answer the following code-related questions within the time limit.  Incorrect answers will penalize your score by 10 seconds."
 document.getElementById("start").textContent = "Start";
 var time = 60;
 
-let questionCount = 0 
+let questionCount = 0
 
 start.addEventListener("click", function (event) {
     title.style.visibility = "hidden";
     start.style.visibility = "hidden";
-   
+    document.getElementById("question").textContent = questions[questionCount].question;
 
-    document.getElementById("question").textContent = questions[questionCount].question ;
-for (let i = 0; i < 4; i++) {
-    document.getElementById(`answer${i}`).textContent = questions[questionCount].answers[i].a;
+    for (let i = 0; i < 4; i++) {
+        document.getElementById(`answer${i}`).textContent = questions[questionCount].answers[i].a;
 
-}
-questionCount = questionCount += 1
+    }
+    questionCount = questionCount += 1
+
+    // change data-display from hidden to block
+    display.setAttribute("style", "display:block")
+
 
     // timer function for countdown
     timer.textContent = "60 seconds"
@@ -43,8 +48,8 @@ questionCount = questionCount += 1
 
 });
 
-answer1.addEventListener("click", function (){
-    document.getElementById("question").textContent = questions[questionCount].question ;
+answer1.addEventListener("click", function () {
+    document.getElementById("question").textContent = questions[questionCount].question;
     for (let i = 0; i < 4; i++) {
         document.getElementById(`answer${i}`).textContent = questions[questionCount].answers[i].a;
     }
