60209eebb7566d5e943da74de6018845e4829c75
Chris Tolnai <christolnai@gmail.com>
2021-08-16 17:16:03 -0700

    questions & answers and countdown timer working

---
+++ assets/js/questions.js
@@ -0,0 +1,46 @@
+var questions = [
+    {
+        question: "Which method removes the last element from an array and returns the element?",
+        answers: [
+            {
+                a: ".pop()",
+                correct: true
+            },
+            {
+                a: ".get()",
+                correct: false
+            },
+            {
+                a: ".sort()",
+                correct: false
+            },
+            {
+                a: ".last()",
+                correct: false
+            }
+        ],
+        
+    },
+    {
+        question: "Which function of Array objects adds one or more elements to the end of an array and returns the new length of the array? ",
+        answers: [
+            {
+                a: ".pop()",
+                correct: false
+            },
+            {
+                a: ".join()",
+                correct: false
+            },
+            {
+                a: ".push()",
+                correct: true
+            },
+            {
+                a: ".map()",
+                correct: false
+            }
+        ],
+        
+    },
+]
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,51 +1,54 @@
 var score = [];
 var start = document.querySelector("#start");
 var main = document.querySelector(main);
+var answer0 = document.querySelector("#answer0");
 var answer1 = document.querySelector("#answer1");
 var answer2 = document.querySelector("#answer2");
 var answer3 = document.querySelector("#answer3");
-var answer4 = document.querySelector("#answer4");
 var correctwrong = document.querySelector("#correctwrong");
-var timercountdown = document.querySelector("#timer");
+var timer = document.querySelector("#timer");
 var container = document.querySelector(".container");
 
 document.getElementById("title").textContent = "Coding Quiz Challenge";
-document.getElementById("question").textContent = "Try to answer the following code-related questions within the time limit.  Incorrect answers will penalize your score by 10 seconds." 
-document.getElementById("start").textContent ="Start";
+document.getElementById("question").textContent = "Try to answer the following code-related questions within the time limit.  Incorrect answers will penalize your score by 10 seconds."
+document.getElementById("start").textContent = "Start";
+var time = 60;
 
-start.addEventListener("click", function (event){
-    event.preventDefault();
+let questionCount = 0 
+
+start.addEventListener("click", function (event) {
     title.style.visibility = "hidden";
-    document.getElementById("question").textContent = "What is 1 + 1 ?";
     start.style.visibility = "hidden";
-    answer1.style.visibility = "visible";
-    document.getElementById("answer1").textContent = "3";
-    document.getElementById("answer2").textContent = "2";
-    document.getElementById("answer3").textContent = "11";
-    document.getElementById("answer4").textContent = "1";
+   
+
+    document.getElementById("question").textContent = questions[questionCount].question ;
+for (let i = 0; i < 4; i++) {
+    document.getElementById(`answer${i}`).textContent = questions[questionCount].answers[i].a;
+
+}
+questionCount = questionCount += 1
+
+    // timer function for countdown
+    timer.textContent = "60 seconds"
+    var timerFunction =
+        setInterval(function () {
+            time--;
+            timer.textContent = time + " seconds";
+
+            if (time <= 0) {
+                clearInterval(timerFunction)
+            }
+        }, 1000);
 
 
 });
 
-// answer1.addEventListener("click", function (){
-//     document.getElementById("answer1").
-// });
+answer1.addEventListener("click", function (){
+    document.getElementById("question").textContent = questions[questionCount].question ;
+    for (let i = 0; i < 4; i++) {
+        document.getElementById(`answer${i}`).textContent = questions[questionCount].answers[i].a;
+    }
+    questionCount = questionCount += 1
 
+});
 
-
-
-// document.querySelector("#question").setAttribute("style" , "font-size:1rem" )
-
-// function startTimer (duration, display){
-    //    var timer = seconds;
-    //     setInterval(function (){
-    //         seconds = parseInt(timer/60, 10);
-            
-    //         display.textContent = timer
-    
-    //         if (--timer < 0){
-    //             timer=duration;
-    //         }
-    //     }, 1000);
-    
-    // }
